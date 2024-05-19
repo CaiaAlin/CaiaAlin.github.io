acf3f4a4844b58681173d4c658c1f9c1d4f36f26
CaiaAlin <caia_alin98@yahoo.com>
2024-05-19 11:56:27 +0200

    uploading web

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+    btn.style.backgroundColor = '#E6752F';
+    btn.style.boxShadow = '0 0 20px #E6752F';
+    btn.style.transition = '.5s ease';
+});
